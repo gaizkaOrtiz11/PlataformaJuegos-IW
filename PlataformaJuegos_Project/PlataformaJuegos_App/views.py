3158cebf7ad85a2cf9ad6d8caf61f794347b1524
angelgandarias <angel.gandarias@opendeusto.es>
2025-10-30 09:32:56 +0100

    HTML lista_plataformas
    HTML básico lista plataformas

--- PlataformaJuegos_Project/PlataformaJuegos_App/views.py
+++ PlataformaJuegos_Project/PlataformaJuegos_App/views.py
@@ -7,12 +7,12 @@ def home(request):
 
 def lista_plataformas(request):
     plataformas = Plataforma.objects.order_by("fecha_lanzamiento")
-    nombres_empresas =','.join([plataforma.compañia for plataforma in plataformas])
-    return render(request, "lista_plataformas.html", {"plataformas": plataformas, "nombres_empresas": nombres_empresas})
+    nombres_plataformas =','.join([plataforma.companyia for plataforma in plataformas])
+    return render(request, "plataforma/lista_plataformas.html", {"plataformas": plataformas})
 
 def detalle_plataforma(request, plataforma_id):
     plataforma = Plataforma.objects.get(id=plataforma_id)
     nombre = plataforma.nombre
     logo = plataforma.logo.url if plataforma.logo else None
     companyia = plataforma.companyia
-    return render(request, "detalle_plataforma.html", {"nombre": nombre, "logo": logo, "companyia": companyia})
+    return render(request, "plataforma/detalle_plataforma.html", {"nombre": nombre, "logo": logo, "companyia": companyia})
