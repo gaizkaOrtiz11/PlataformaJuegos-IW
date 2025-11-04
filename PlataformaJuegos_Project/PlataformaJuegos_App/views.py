c13c24640a7f0253e474eddfcb673010af47ddeb
angelgandarias <angel.gandarias@opendeusto.es>
2025-11-04 13:24:33 +0100

    Update views (fixed bug)

--- PlataformaJuegos_Project/PlataformaJuegos_App/views.py
+++ PlataformaJuegos_Project/PlataformaJuegos_App/views.py
@@ -9,7 +9,7 @@ def home(request):
 def lista_plataformas(request):
     plataformas = Plataforma.objects.order_by("fecha_lanzamiento")
     nombres_empresas =','.join([plataforma.companyia for plataforma in plataformas if plataforma.companyia])
-    return render(request, "lista_plataformas.html", {"plataformas": plataformas, "nombres_empresas": nombres_empresas})
+    return render(request, "plataforma/lista_plataformas.html", {"plataformas": plataformas, "nombres_empresas": nombres_empresas})
 
 def detalle_plataforma(request, plataforma_id):
     plataforma = Plataforma.objects.get(id=plataforma_id)
