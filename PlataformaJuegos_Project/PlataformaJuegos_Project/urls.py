5e0761b4470dd99b4ff394b5ad5d21b2a59e4d61
Gaizka Ortiz <gaizka.ortiz@opendeusto.es>
2025-11-06 09:36:13 +0100

    datos

--- PlataformaJuegos_Project/PlataformaJuegos_Project/urls.py
+++ PlataformaJuegos_Project/PlataformaJuegos_Project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("plataformaApp/", include("PlataformaJuegos_App.urls"))
+    path("plataformaApp/", include("PlataformaJuegos_App.urls")),
+    path("plataformas/", include("PlataformaJuegos_App.urls")),
 ]
