1c9e7b5c42d80d52c43b2def362fe3f43ce2b12c
4aac <management.asher@gmail.com>
2025-11-08 12:18:16 +0100

    completely fixed

--- PlataformaJuegos_Project/PlataformaJuegos_App/views.py
+++ PlataformaJuegos_Project/PlataformaJuegos_App/views.py
@@ -3,13 +3,13 @@ from .models import Plataforma, Juego, Jugador
 
 # Create your views here.
 def home(request):
-    return HttpResponse(request, "index.html")
+    return render(request, "index.html")
 
 
 def lista_plataformas(request):
     plataformas = Plataforma.objects.order_by("fecha_lanzamiento")
     nombres_empresas =','.join([plataforma.companyia for plataforma in plataformas if plataforma.companyia])
-    return render(request, "lista_plataformas.html", {"plataformas": plataformas, "nombres_empresas": nombres_empresas})
+    return render(request, "plataforma/lista_plataformas.html", {"plataformas": plataformas, "nombres_empresas": nombres_empresas})
    
 
 def detalle_plataforma(request, pk):
@@ -20,7 +20,7 @@ def detalle_plataforma(request, pk):
 def lista_juegos(request):
     juegos = Juego.objects.order_by("nombre")
     nombres_empresas = ','.join([juego.companyia for juego in juegos if juego.companyia])
-    return render(request, "lista_juegos.html", {"juegos": juegos, "nombres_empresas": nombres_empresas})
+    return render(request, "juego/lista_juegos.html", {"juegos": juegos, "nombres_empresas": nombres_empresas})
 
 def detalle_juego(request, pk):
     juego = Juego.objects.get(id=pk)
@@ -30,7 +30,7 @@ def detalle_juego(request, pk):
 def lista_jugadores(request):
     jugadores = Jugador.objects.order_by("apodo")
     apodos = ','.join([jugador.apodo for jugador in jugadores if jugador.apodo])
-    return render(request, "lista_jugadores.html", {"jugadores": jugadores, "apodos": apodos})
+    return render(request, "jugador/lista_jugadores.html", {"jugadores": jugadores, "apodos": apodos})
 
 def detalle_jugador(request, pk):
     jugador = Jugador.objects.get(id=pk)
