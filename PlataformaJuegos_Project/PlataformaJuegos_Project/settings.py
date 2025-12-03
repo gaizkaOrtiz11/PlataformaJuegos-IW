df35500316baf35657ad7348aea85e6c910d9d92
Gaizka Ortiz <gaizka.ortiz@opendeusto.es>
2025-12-03 20:45:46 +0100

    Funcionalidad principal
    Funcionalidad de la traduccion sin crear los archivos de traduccion

--- PlataformaJuegos_Project/PlataformaJuegos_Project/settings.py
+++ PlataformaJuegos_Project/PlataformaJuegos_Project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',# Middleware para la traducción
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -103,13 +104,22 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es' 
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+#Idiomas soportados
+LANGUAGES = [
+    ('es', 'Spanish'),
+    ('en', 'English'),
+]
+#Ruta de los archivos de traducción .po
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',  
+]
 
 
 # Static files (CSS, JavaScript, Images)
--- PlataformaJuegos_Project/PlataformaJuegos_Project/urls.py
+++ PlataformaJuegos_Project/PlataformaJuegos_Project/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+]
+urlpatterns += [
     path('admin/', admin.site.urls),
     path("plataformaApp/", include("PlataformaJuegos_App.urls")),
     path("plataformas/", include("PlataformaJuegos_App.urls")),
